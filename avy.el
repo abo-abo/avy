e242f04e32c7d874c779fb83c86aa5bdbc508f18
Oleh Krehel <ohwoeowho@gmail.com>
2015-05-09 13:47:07 +0200

    Bump version

--- avy.el
+++ avy.el
@@ -4,7 +4,7 @@
 
 ;; Author: Oleh Krehel <ohwoeowho@gmail.com>
 ;; URL: https://github.com/abo-abo/avy
-;; Version: 0.1.0
+;; Version: 0.2.0
 ;; Package-Requires: ((emacs "24.1") (cl-lib "0.5"))
 ;; Keywords: point, location
 
