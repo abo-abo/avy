c0db3642d65eba76df8c7ee5c98042faecf79c4d
Emmanuel Denloye-Ito <eod8tb@virginia.edu>
2016-04-13 23:42:18 -0400

    Add avy-action-ispell to avy.el
    The point should return to its starting location after the desired word
    is corrected. As is the case for the other avy-action-{something}
    functions, this function is also added to the avy-dispatch-alist.
    
    Fixes #142

--- avy.el
+++ avy.el
@@ -523,6 +523,17 @@ Set `avy-style' according to COMMMAND as well."
    (just-one-space))
   (message "Killed: %s" (current-kill 0)))
 
+(defun avy-action-ispell (pt)
+  "Auto correct word at PT."
+  (save-excursion
+    (goto-char pt)
+    (if (looking-at-p "\\b")
+        (ispell-word)
+      (progn
+        (backward-word)
+        (when (looking-at-p "\\b")
+          (ispell-word))))))
+
 (defun avy--process (candidates overlay-fn)
   "Select one of CANDIDATES using `avy-read'.
 Use OVERLAY-FN to visualize the decision overlay."
